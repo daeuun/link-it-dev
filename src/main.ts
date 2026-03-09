4a924fd1812b2d870900fa43565f012f97e8e726
daeuun <leeda51925@gmail.com>
2026-03-09 16:41:58 +0900

    feat: 검증 파이프 설정 추가 및 CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { AppModule } from './app.module';
@@ -7,9 +8,30 @@ import { setSwaggerConfig } from './common/config/swagger.config';
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  // Swagger 설정
+  setupGlobalPipes(app);
   setSwaggerConfig(app);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
+
+/** * 전역 검증 파이프 설정
+ */
+ function setupGlobalPipes(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되어 있지 않은 속성 제거 
+      forbidNonWhitelisted: true, // 허용되지 않은 속성이 포함된 요청 거부
+      transform: true, // DTO에 정의된 타입으로 변환
+      transformOptions: { enableImplicitConversion: true }, // 타입 변환
+    }),
+  );
+}
+
+/**
+ * 보안 설정 (CORS)
+ */
+function setupSecurity(app: INestApplication) {
+  app.enableCors();
+}
