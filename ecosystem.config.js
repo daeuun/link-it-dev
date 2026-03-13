2c3bad93bee951d460332c6a31f006c534d752f7
daeuun <leeda51925@gmail.com>
2026-03-13 10:17:45 +0900

    fix: 서버 노드 버전 사용하도록 interpreter 지정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: 'dist/main.js',
     instances: 1,
     exec_mode: 'fork',
+    interpreter: 'node', // 시스템 기본 노드로 지정
     error_file: './log/error.log',
     out_file: './log/access.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
